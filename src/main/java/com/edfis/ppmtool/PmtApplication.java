997c5c54c1918fdb0abb53f7164eae0dcb5fad38
EduardF1 <fischer_eduard@yahoo.com>
2021-09-18 00:40:30 +0200

    change configuration

--- src/main/java/com/edfis/ppmtool/PmtApplication.java
+++ src/main/java/com/edfis/ppmtool/PmtApplication.java
@@ -3,10 +3,11 @@ package com.edfis.ppmtool;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class PmtApplication {
 
     @Bean
